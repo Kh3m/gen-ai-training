010766b570822a167f0be40966997fd009551a97
Khem Khem <khem6333@gmail.com>
2026-03-15 00:28:15 +0100

    Add streamlit

---
+++ streamlit/core.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Love To Code")
+df = pd.DataFrame([
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+], columns=["A", "B", "C", "D", "E"])
+
+st.write(df)
