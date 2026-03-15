81a98db8ec70983bdd2009c3fa4ac6063182b9b2
Khem Khem <khem6333@gmail.com>
2026-03-15 20:03:06 +0100

    Add more widgets

--- streamlit/core.py
+++ streamlit/core.py
@@ -1,10 +1,26 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("Love To Code")
+
+
+st.write("Pandas DataFrames",)
+
 df = pd.DataFrame([
     [1,2,3,4,5],
     [6,7,8,9,10],
 ], columns=["A", "B", "C", "D", "E"])
 
+people_df = pd.DataFrame({
+    "Name": ["Abdulkareem", "Khem", "Adams"],
+    "Age": [23,12, 32],
+})
+
 st.write(df)
+st.write(people_df)
+
+# Create a chart - line
+
+data = np.random.randn(20, 3)
+st.line_chart(data)
