75604a6b21227b8ae36874ab021e51f2a2e54ff5
Khem Khem <khem6333@gmail.com>
2026-03-15 20:03:06 +0100

    Add more widgets

--- streamlit/widgets.py
+++ streamlit/widgets.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
 
 name = st.text_input("Enter your name: ")
 
 if name:
     st.write("Your name is: ", name)
+
+price=st.slider("Enter price range: ", 0, 100, 50)
+
+st.write("You entered: ", price)
+
+options = ["Male", "Female"]
+sex = st.selectbox("Select Gender: ", options)
+
+st.write("Your Gender: ", sex)
+
+upload_file = st.file_uploader("Choose a CSV file", type="csv")
+ 
+if upload_file:
+    uploaded_csv = pd.read_csv(upload_file)
+    st.write("You Uploaded: ")
+    st.write(uploaded_csv)
+
+
