2f01d3c7b2da77ed52128a26d6fc839c41c36725
Kh3m <khem6333@gmail.com>
2026-06-21 08:56:30 +0100

    Remove comment

--- genai/openai-api/main.py
+++ genai/openai-api/main.py
@@ -42,22 +42,3 @@ while True:
     print("="*10)
     print(output_data)
     print("="*10)
-
-
-
-# import os
-# from openai import OpenAI
-
-# client = OpenAI(
-#     api_key=os.environ["GROQ_API_KEY"],
-#     base_url="https://api.groq.com/openai/v1"
-# )
-
-# response = client.chat.completions.create(
-#     model="llama-3.3-70b-versatile",
-#     messages=[
-#         {"role": "user", "content": "Write a short bedtime story about a unicorn."}
-#     ]
-# )
-
-# print(response.choices[0].message.content)
